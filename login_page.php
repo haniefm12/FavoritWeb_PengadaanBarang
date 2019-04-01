caee3fba4596cb46532cc9b61a5e88c2d5e74a4c
adorabable <whatthefapmc@gmail.com>
2019-04-02 00:12:30 +0700

    login page

--- login_page.php
+++ login_page.php
@@ -2,10 +2,32 @@
 <html>
 <head>
 	<title> </title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- Custom Theme files -->
+	<link href="assets/css/styleLogin.css" rel="stylesheet" type="text/css" media="all" />
+<!-- //Custom Theme files -->
+<!-- web font -->
+	<link href="//fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,700,700i" rel="stylesheet">
+<!-- //web font -->
 </head>
-<body >
-<!-- semnagt nando frontendnya, gausah disamain dulu sama backendnya yaa, itu cuma buat garis haluan urg -->
+<body>
 
+<!-- main -->
+	<div class="main-w3layouts wrapper">
+		<h1>Welcome</h1>
+		<div class="main-agileinfo">
+			<div class="agileits-top">
+				<form action="#" method="post">
+					<input class="text" type="text" name="Username" placeholder="Username" required=""><br>
+					<input class="text" type="password" name="password" placeholder="Password" required=""><br>
+					<input type="submit" value="LOGIN">
+				</form>
+			</div>
+		</div>
+	</div>
+	<!-- //main -->
 
 <?php
 	
